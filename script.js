e11e6d5848ced4627efcda0470745c86924ee078
skerdinelaj <nelajkeno@gmail.com>
2024-05-16 16:31:22 +0200

    finished portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleMenu = () => {
+    const icon = document.querySelector('.hamburger-icon');
+    const menu = document.querySelector('.menu-links');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
